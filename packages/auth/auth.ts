3b45b581c8755a5e68f871d582dfd7ec14aeecfa
Subhdeep pal <subhdeeppal7@gmail.com>
2026-08-08 16:14:09 +0530

    new commit

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -4,10 +4,10 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 console.log(process.env.BETTER_AUTH_FRONTEND_URL)
 export const auth = betterAuth({
   trustedOrigins: [
-    process.env.NEXT_PUBLIC_BETTER_AUTH_FRONTEND_URL!,
+    process.env.BETTER_AUTH_FRONTEND_URL!,
   ],
 
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
+  baseURL: process.env.BETTER_AUTH_BASE_URL!,
 
   advanced: {
     useSecureCookies: true,
